1eba97aa9187515379e949bec80e6e0028d947c7
anmolcheema836 <anmolcheema988@gmail.com>
2025-04-06 14:05:08 +0530

    gsgffd

--- script.js
+++ script.js
@@ -424,7 +424,7 @@ function loadTicTacToe() {
     if (result === human)
       statusDisplay.textContent = "You win! (Unexpected)";
     else if (result === computer)
-      statusDisplay.textContent = "Sorry! Anmol Cheema never loses.";
+      statusDisplay.textContent = "80% Discount! If someone wins and sends me the screenshot 😉.";
     else statusDisplay.textContent = "It's a draw!";
     addRetryButton(loadTicTacToe);
   }
